7043e76431d5cba2acef9f9021eb5b4ce1d60d46
Anny Anny <58954581+anyt04ka@users.noreply.github.com>
2021-12-14 21:23:30 +0300

    2 задачи внутри

---
+++ hw_3/js/hw_3.1.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let articles =  [
+    {
+        title: "Нейросеть может читать текст быстрее человека",
+        image: "https://picsum.photos/id/22/1000/1000"
+    },
+    {
+        title: "Пять сервисов, которые помогут написать статью за 5 минут",
+        image: "https://picsum.photos/id/24/1000/1000"
+    },
+    {
+        title: "Названы основные правила движения поездов",
+        image: "https://picsum.photos/id/28/1000/1000"
+    },
+    {
+        title: "Самая посещаемая достопримечательность мира",
+        image: "https://picsum.photos/id/27/1000/1000"
+    },
+    {
+        title: "Появился новый термин для обозначения радиоволн",
+        image: "https://picsum.photos/id/29/1000/1000"
+    },
+    {
+        title: "В России появится новый вид бумаги",
+        image: "https://picsum.photos/id/30/1000/1000"
+    }
+];
+
+let app = Vue.createApp({
+    data(){
+        return {
+            articles,
+            input: "",
+        }
+    },
+    computed: {
+        allArticles(){
+            return this.articles.filter(article => {
+                return article.title.toLowerCase().includes(this.input.toLowerCase())
+            })
+        }
+    },
+    methods: {
+
+    }
+});
+
+app.mount("#app");
